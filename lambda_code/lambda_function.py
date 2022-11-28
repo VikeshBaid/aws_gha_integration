ac6609a2419106e8977e828003c8ad4864d88b56
Vikesh Baid <vikesh0610@gmail.com>
2022-11-28 11:09:37 +0530

    update for shodan pipeline

--- lambda_code/lambda_function.py
+++ lambda_code/lambda_function.py
@@ -1,10 +1,27 @@
+import boto3
 import json
+import hashlib
+import hmac
+import datetime
+import os
 
+SECRET_NAME = "TEST/sandbox/shodan-test-api"
+SM_CLIENT = boto3.client('secretsmanager')
+SECRET_VALUES = json.loads(
+    SM_CLIENT.get_secret_value(SecretId=SECRET_NAME).get('SecretString', "{}")
+)
+
+def get_shodan_api_key():
+    return SECRET_VALUES.get('api_key')
 
 def lambda_handler(event, context):
-    print("rest api ")
-    print(event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+    my_key = get_shodan_api_key()
+    print(my_key)
+#     print("rest api ")
+#     print(event)
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
+
